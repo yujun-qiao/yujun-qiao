5c2c57161de7e0043ba9c2e41f933e4f58b28d37
yujun-qiao <3094467875@qq.com>
2024-10-29 20:55:59 +0800

    Update index.py

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ st.title("AI大模型应用产品网")
 col,col1=st.columns(2)
 #语言大模型应用程序入口
 with col:
-    st.image("OlP-C.jfif",use_column_width=True)#图片尽量找大小一样的
+    st.image("OIP-C.jfif",use_column_width=True)#图片尽量找大小一样的
     flag=st.button("御乔畅言",use_container_width=True)
     if flag:
         st.switch_page("pages/youhui.py")
